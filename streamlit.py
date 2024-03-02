dbffd9c3f058b0457928c0971308157c6429824f
gkamboj5 <136140504+gkamboj5@users.noreply.github.com>
2024-03-01 22:28:09 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from dotenv import load_dotenv
 
+load_dotenv()
+
+prompt: str = st.chat_input("Enter a prompt here")
+
+USER = "user"
+ASSISTANT = "assistant"
+
+if prompt:
+    st.chat_message(USER).write(prompt)
+    st.chat_message(ASSISTANT).write(f"You wrote {prompt}")
